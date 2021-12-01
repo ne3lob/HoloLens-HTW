25a378a16ce4d7747d7c4ce651baf02c2d32d64f
Warja Rybakova <s0562770@htw-berlin.de>
2021-12-01 15:43:05 +0100

    all objects in Box are with materials and tooltips

---
+++ Assets/DisabledCollidersOnSicherungkastenObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DisabledCollidersOnSicherungkastenObjects : MonoBehaviour
+{
+    public void TurnOnSicherungkastenCollider()
+    {
+        GameObject.Find("CaseSicherungkaste").GetComponent<MeshCollider>().enabled = true;
+        GameObject.Find("DoorSicherungkaste").GetComponent<BoxCollider>().enabled = true;
+        GameObject.Find("BackgroundCaseSicherungkaste").GetComponent<MeshCollider>().enabled = true;
+    }
+
+    public void TurnOffSicherungkastenCollider()
+    {
+        GameObject.Find("CaseSicherungkaste").GetComponent<MeshCollider>().enabled = false;
+        GameObject.Find("DoorSicherungkaste").GetComponent<BoxCollider>().enabled = false;
+        GameObject.Find("BackgroundCaseSicherungkaste").GetComponent<MeshCollider>().enabled = false;
+    }
+}
